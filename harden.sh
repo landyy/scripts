852afb1643ea83bcb197b673129e6ae68ef7ce2e
root <rmb2555@rit.edu>
2017-02-26 15:26:46 -0800

    Added more features to harden

--- harden.sh
+++ harden.sh
@@ -1,48 +1,117 @@
 #!/bin/bash
 
+clear
+
+echo "Starting harden.sh..."
+echo ""
+echo ""
+sleep 2
+
+#checking for more accs with UID of 0
+uid=$(awk -F: '($3 == 0) {print $1}' /etc/passwd)
+root="root"
+if [[ $uid != $root ]]; then
+	echo ""
+	echo "################################################"
+	echo "# WARNING: More than one account with UID of 0 #"
+	echo "################################################"
+	echo ""
+	echo "$root"
+	echo ""
+
+fi
+
+#checking if users have null passwords
+pass=$(awk -F: '($2 == "") {print $1}' /etc/shadow)
+if [[ $pass != ""  ]]; then
+	echo ""
+	echo "################################################"
+	echo "# WARNING: Account with null password found    #"
+	echo "################################################"
+	echo ""
+	echo "$pass"
+	echo ""
+fi
+
+
 #checking permissions
 if [ -d "/var/www/" ]; then
-	if [ stat -c "%a" /var/www/html != 700]; then
-		echo "Bad Permissions in /var/www/. Fixing..."
-		chmod 700 -R /var/www/
+	if [ $(stat -c "%a" /var/www) != 755 ]; then
+		echo ""
+		echo "########################################"
+		echo "# Bad Permissions in /var/www/, fixing #"
+		echo "########################################"
+		echo ""
+		chmod 755 /var/www/
+		chmod 644 /var/www/html
+	fi
+
+	if [ $(stat -c "%a" /var/www/html) != 644 ]; then
+ 	
+		echo ""
+		echo "############################################"
+		echo "# Bad Permissions in /var/www/html, fixing #"
+		echo "############################################"
+		echo ""
+		chmod 755 /var/www/
+		chmod 644 /var/www/html
 	fi
 	
 fi
 
 #checking default port for ssh
-if [ cat /etc/ssh/sshd_config | grep 22 != ""]; then
-	echo "Default Port for SSH in use, fixing"
-	sed -i 's/Port 22/Port 3434/' /etc/ssh/sshd_config
+if [ -d "/etc/ssh/" ]; then
+	if [ "$(cat /etc/ssh/sshd_config | grep 22)" ]; then
+		echo ""
+		echo "#######################################"
+		echo "# Default Port for SSH in use, fixing #"
+		echo "#######################################"
+		echo ""
+		sed -i 's/Port 22/Port 3434/' /etc/ssh/sshd_config
+	fi
 fi
 
 #checking ftp anon login
 if [ -d "/etc/vstpd.conf" ]; then
-	if [ cat /etc/vsftpd.conf | grep anonymous_enable=YES ]; then
+	if [ $(cat /etc/vsftpd.conf | grep anonymous_enable=YES) ]; then
+		echo ""
+		echo "#######################################"
+		echo "# Anon login enabled for ftp, fixing  #"
+		echo "#######################################"
+		echo ""
 		sed -i 's/anonymous_enable=YES/anonymous_enable=NO/' /etc/vsftpd.conf
 	fi
 fi
 
 #DNS permissions check
 if [ -d "/var/named" ];then
-	echo "/var/named exsists"
+	echo ""
+	echo "#######################################"
+	echo "# /var/named already exsists          #"
+	echo "#######################################"
+	echo ""
 else
 	mkdir /var/named
+	chmod 770 named
 fi
-chmod 770 named
 
-if [ -d "/var/run/named.pid" ];then
-	echo "/var/run/named.pid"
-else
+#check for named.pid
+if [ ! -d "/var/run/named.pid" ];then
 	touch /var/run/named.pid
 fi
 
-chown named:named /var/run/named.pid
-
+#move the chattr command
+#mv /usr/bin/chattr /usr/bin/yeezy
+#touch chattr
+#touch notchattr
+#touch nothinghere_:)
+#touch notcat
+#touch stopit
 
 #Making certain files immutiable 
-chattr +i /etc/passwd
-chattr +i /etc/shadow
-chattr +i /etc/ssh/sshd_config
+#yeezy +i /etc/passwd
+#yeezy +i /etc/shadow
+#yeezy +i /etc/ssh/sshd_config
 
 
 
